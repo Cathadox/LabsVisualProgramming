7e1208351148458086bd9b8fead7f5ea7d417960
Cathadox <dimitrij.dobrevski@gmail.com>
2018-04-25 03:43:17 +0200

    8th Laboratory Exam

---
+++ Pacman1/Pacman1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+using Pacman1.Properties;
+
+namespace Pacman1
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        Pacman pacman;
+        static readonly int TIMER_INTERVAL = 250;
+        static readonly int WORLD_WIDTH = 15;
+        static readonly int WORLD_HEIGHT = 10;
+        Image foodImage;
+        bool[][] foodWorld;
+        public Form1()
+        {
+            InitializeComponent();
+            foodImage = Resources.peach;
+            DoubleBuffered = true;
+            newGame();
+        }
+        public void newGame()
+        {
+            pacman = new Pacman();
+            this.Width = Pacman.Radius * 2 * (WORLD_WIDTH + 1);
+            this.Height = Pacman.Radius * 2 * (WORLD_HEIGHT + 1);
+            ////////////////////////////
+            foodWorld = new bool[WORLD_HEIGHT][];
+            for (int i = 0; i < WORLD_HEIGHT; i++)
+                foodWorld[i] = new bool[WORLD_WIDTH];
+            for (int i = 0; i < WORLD_HEIGHT; i++)
+                for (int j = 0; j < WORLD_WIDTH; j++)
+                    foodWorld[i][j] = true;
+            ////////////////////////////
+            timer = new Timer();
+            timer.Interval = TIMER_INTERVAL;
+            timer.Tick += new EventHandler(timer_tick);
+            timer.Start();
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            foodWorld[pacman.y-1][pacman.x-1] = false;
+            pacman.Move(WORLD_WIDTH, WORLD_HEIGHT);
+            Invalidate();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Right:
+                    pacman.ChangeDirection(1);
+                    break;
+                case Keys.Left:
+                    pacman.ChangeDirection(2);
+                    break;
+                case Keys.Up:
+                    pacman.ChangeDirection(3);
+                    break;
+                case Keys.Down:
+                    pacman.ChangeDirection(4);
+                    break;
+                default:
+                    break;
+                
+            }
+
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            for (int i = 0; i < foodWorld.Length; i++)
+            {
+                for (int j = 0; j < foodWorld[i].Length; j++)
+                {
+                    if (foodWorld[i][j])
+                    {
+                        g.DrawImageUnscaled(foodImage, j * Pacman.Radius * 2 + (Pacman.Radius * 2 - foodImage.Height) / 2, i * Pacman.Radius * 2 + (Pacman.Radius * 2 - foodImage.Width) / 2);
+                    }
+                }
+            }
+            pacman.Draw(g);
+        }
+    }
+}
---
+++ Pacman1/Pacman1/Pacman.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Pacman1
+{
+    public class Pacman
+    {
+        public enum Direction
+        {
+            LEFT,
+            UP,
+            RIGHT,
+            DOWN
+        }
+        public Direction direction;
+        private int X { get; set; }
+        private int Y { get; set; }
+        public static readonly int Radius = 20;
+        private static int Velocity;
+        private bool mouth { get; set; }
+        public int x = 5;
+        public int y = 7;
+        Brush brush = new SolidBrush(Color.Yellow);
+        public Pacman()
+        {
+            Velocity = Radius;
+            X = 7;
+            Y = 5;
+            direction = Direction.RIGHT;
+        }
+        public void Draw(Graphics g)
+        {
+            Rectangle rec = new Rectangle(X - Radius, Y - Radius, 2 * Radius, 2 * Radius);
+            if(mouth && direction == Direction.RIGHT)
+            {
+                g.FillPie(brush, rec, 135, 225);
+            }
+            if(mouth && direction == Direction.LEFT)
+            {
+                g.FillPie(brush, rec, 45, 315);
+            }
+            if(mouth && direction == Direction.UP)
+            {
+                g.FillPie(brush, rec, 225, 315);
+            }
+            if(mouth && direction == Direction.DOWN)
+            {
+                g.FillPie(brush, rec, 45, 135);
+            }
+            if(!mouth)
+            {
+                g.FillEllipse(brush, rec);
+            }
+        }
+        public void Move(int i, int j)
+        {
+            if(direction == Direction.RIGHT)
+            {
+                if (X == i)
+                {
+                    X = 1;
+                    
+                }
+                else
+                { X += Velocity; }
+                
+            }
+            if (direction == Direction.LEFT)
+            {
+                if (X == 1)
+                { X = i; }
+                else
+                    X -= Velocity;
+            }
+            if (direction == Direction.DOWN)
+            {
+                if (Y == j)
+                    Y = 1;
+                else
+                    Y++;
+            }
+            if (direction == Direction.UP)
+            {
+                if (Y == 1)
+                    Y = j;
+                else
+                    Y--;
+            }
+            if(mouth)
+            {
+                mouth = false;
+            }
+            else
+            {
+                mouth = true;
+            }
+
+        }
+        public void ChangeDirection(int i)
+        {
+            if (i == 1)
+                direction = Direction.RIGHT;
+            if (i == 2)
+                direction = Direction.LEFT;
+            if (i == 3)
+                direction = Direction.UP;
+            if (i == 4)
+                direction = Direction.DOWN;
+        }
+    }
+}
